81b04a4da8c63f8d5e96732b668fd66e40fd7ad8
Mikaela Rhoads <98623081+open-rhoads@users.noreply.github.com>
2025-05-28 16:33:51 -0700

    make common scripts dynamic

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -24,6 +24,40 @@ function activeButton() { // this function adds/removes the active class dependi
   });
 }
 
+function headScripts() {
+  document.addEventListener('DOMContentLoaded', function () {
+    const scripts = [
+      {
+        src: 'https://code.jquery.com/jquery-3.3.1.slim.min.js',
+        integrity:
+          'sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo',
+        crossorigin: 'anonymous',
+      },
+      {
+        src: 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js',
+        integrity:
+          'sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut',
+        crossorigin: 'anonymous',
+      },
+      {
+        src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js',
+        integrity:
+          'sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO',
+        crossorigin: 'anonymous',
+      },
+    ];
+
+    scripts.forEach(({ src, integrity, crossorigin }) => {
+      const script = document.createElement('script');
+      script.src = src;
+      if (integrity) script.integrity = integrity;
+      if (crossorigin) script.crossOrigin = crossorigin;
+      script.defer = true;
+      document.body.appendChild(script);
+    });
+  });
+}
+
 function headerFooter() {
   document.addEventListener("DOMContentLoaded", function () {
     const headerHTML = `
@@ -70,6 +104,7 @@ function headerFooter() {
 }
 
 function init() {
+  headScripts();
   headerFooter();
   activeButton();
 }
